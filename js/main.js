c5873a5b359239f38c7e8d1183665e30f7890851
DDushkin <minuswant@gmail.com>
2020-01-24 13:00:57 +0200

    Added secret lvl

--- js/main.js
+++ js/main.js
@@ -1,36 +1,64 @@
 $(function() {
-    console.log( "ready!" );
-    var clickCount = 0;
+  const body = document.body;
+  let clickCount = 0;
+  let game = '';
+  let isMobile = window.matchMedia("only screen and (min-width: 992px)").matches;
 
-    $('div.mario').click(function(){
-      clickCount++;
-      console.log(clickCount);
-    	let mario = $(this);
-    	mario.find('div.box').css('top','-5px');
+  window.addEventListener('blur', () => {
+    if (document.activeElement === game) {
+      body.style.overflow = 'hidden'
+    } else {
+      body.style.overflow = 'visible'
+    }
+  })
 
-      if (clickCount === 9) {
+  window.addEventListener('focus', () => {
+    body.style.overflow = 'visible'
+  })
+
+  $('div.mario').click(function() {
+    clickCount++;
+    let mario = $(this);
+    mario.find('div.box').css('transform', 'translateY(-5px)');
+
+    if (clickCount === 9) {
+      $("<audio></audio>").attr({
+        'src': '../sounds/powerup-appears.wav',
+        'autoplay': 'autoplay'
+      }).appendTo("body");
+      mario.find('span.mushroom').addClass('play');
+
+      setTimeout(function() {
+        mario.find('span.mushroom').removeClass('play');
+      }, 2000);
+    } else if (clickCount === 20 && !isMobile) {
+      $('.game').addClass('screen');
+
+      setTimeout(function() {
+        $('.game').html('<iframe src="https://supermarioemulator.com/mario.php"></iframe>');
+        game = document.querySelector('.game iframe');
         $("<audio></audio>").attr({
-          'src':'../sounds/powerup-appears.wav',
-          'autoplay':'autoplay'
+          'src': '../sounds/secret.wav',
+          'autoplay': 'autoplay'
         }).appendTo("body");
-        mario.find('span.mushroom').addClass('play');
+      }, 500);
 
-        setTimeout(function(){
-          mario.find('span.mushroom').removeClass('play');
-        }, 500);
-      } else {
-        $("<audio></audio>").attr({
-          'src':'../sounds/gold-sound.mp3',
-          'autoplay':'autoplay'
-        }).appendTo("body");
-        mario.find('span.coin').addClass('play');
+      $('html, body').animate({
+        scrollTop: $(".game").offset().top
+      }, 2000);
+    } else {
+      $("<audio></audio>").attr({
+        'src': '../sounds/gold-sound.mp3',
+        'autoplay': 'autoplay'
+      }).appendTo("body");
+      mario.find('span.coin').addClass('play');
 
-        setTimeout(function(){
-          mario.find('span.coin').removeClass('play');
-        }, 500);
-      }
-      setTimeout(function(){
-        mario.find('div.box').css('top','0px');
-      }, 100);
-    });
+      setTimeout(function() {
+        mario.find('span.coin').removeClass('play');
+      }, 500);
+    }
+    setTimeout(function() {
+      mario.find('div.box').css('transform', 'translateY(0px)');
+    }, 100);
+  });
 });
